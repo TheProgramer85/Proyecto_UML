b427e986fa6a20a2bc260a1a1a415b55710f33b0
TheProgramer85 <131801107+TheProgramer85@users.noreply.github.com>
2023-07-02 00:25:38 -0400

    UMLCreator.java
    Ultima actualización Proyecto UML

--- Proyecto_UML/src/main/java/org/example/UMLCreator.java
+++ Proyecto_UML/src/main/java/org/example/UMLCreator.java
@@ -2,6 +2,7 @@ package org.example;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -10,17 +11,16 @@ import java.awt.event.MouseMotionAdapter;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 class UMLCreator extends JFrame implements ActionListener {
     JMenu archivo, dibujar, ayuda;
-    JMenuItem acerca, salir, nuevo, guardar, abrir, color;
-    JRadioButtonMenuItem linea, rectangulo, elipse;
+    JMenuItem acerca, salir, nuevo, guardar, abrir, color, agregarTexto;
+    JRadioButtonMenuItem dibujo, rectangulo, elipse, monigote, estado, generalizacion, asociacion,
+            composicion, agregacion, dependencia;
     JCheckBoxMenuItem relleno;
     JColorChooser colorChooser = new JColorChooser();
     ButtonGroup btn;
@@ -45,9 +45,17 @@ class UMLCreator extends JFrame implements ActionListener {
         guardar.addActionListener(this);
         abrir.addActionListener(this);
         color.addActionListener(this);
-        linea.addActionListener(this);
+        agregarTexto.addActionListener(this);
+        dibujo.addActionListener(this);
+        generalizacion.addActionListener(this);
+        composicion.addActionListener(this);
+        asociacion.addActionListener(this);
+        agregacion.addActionListener(this);
+        dependencia.addActionListener(this);
+        estado.addActionListener(this);
         rectangulo.addActionListener(this);
         elipse.addActionListener(this);
+        monigote.addActionListener(this);
         relleno.addActionListener(this);
     }
 
@@ -63,25 +71,48 @@ class UMLCreator extends JFrame implements ActionListener {
         archivo.add(guardar);
         archivo.add(salir);
         menu.add(archivo);
-        dibujar = new JMenu("Dibujar");
+        dibujar = new JMenu("Elementos");
         btn = new ButtonGroup();
-        linea = new JRadioButtonMenuItem("Linea");
-        rectangulo = new JRadioButtonMenuItem("Rectangulo");
-        elipse = new JRadioButtonMenuItem("Elipse");
+        dibujo = new JRadioButtonMenuItem("Dibujo");
+        generalizacion = new JRadioButtonMenuItem("Generalización");
+        asociacion = new JRadioButtonMenuItem("Asociación");
+        composicion = new JRadioButtonMenuItem("Composición");
+        dependencia = new JRadioButtonMenuItem("Dependencia");
+        agregacion = new JRadioButtonMenuItem("Agregación");
+        rectangulo = new JRadioButtonMenuItem("Clase");
+        elipse = new JRadioButtonMenuItem("Caso de Uso");
+        monigote = new JRadioButtonMenuItem("Monigote");
+        estado = new JRadioButtonMenuItem("Estado");
+        btn.add(estado);
+        btn.add(generalizacion);
         btn.add(elipse);
         btn.add(rectangulo);
-        btn.add(linea);
-        btn.setSelected(linea.getModel(), true);
+        btn.add(asociacion);
+        btn.add(composicion);
+        btn.add(agregacion);
+        btn.add(dependencia);
+        btn.add(dibujo);
+        btn.add(monigote);
+        btn.setSelected(dibujo.getModel(), true);
         relleno = new JCheckBoxMenuItem("Relleno");
         color = new JMenuItem("Color");
-        dibujar.add(linea);
+        agregarTexto = new JMenuItem("Agregar Texto");
+        dibujar.add(dibujo);
+        dibujar.add(generalizacion);
+        dibujar.add(estado);
         dibujar.add(rectangulo);
         dibujar.add(elipse);
+        dibujar.add(asociacion);
+        dibujar.add(composicion);
+        dibujar.add(agregacion);
+        dibujar.add(dependencia);
+        dibujar.add(monigote);
         dibujar.add(relleno);
         dibujar.add(color);
+        dibujar.add(agregarTexto);
         menu.add(dibujar);
         ayuda = new JMenu("Más");
-        acerca = new JMenuItem("Hecho por TheProgramer85");
+        acerca = new JMenuItem("Hecho por Diego Villablanca / TheProgramer85");
         ayuda.add(acerca);
         menu.add(ayuda);
         this.setJMenuBar(menu);
@@ -108,8 +139,8 @@ class UMLCreator extends JFrame implements ActionListener {
         if (e.getSource() == acerca) {
             JOptionPane.showMessageDialog(null, "Este programa ha sido creado por TheProgramer85");
         }
-        if (e.getSource() == linea) {
-            miPanel.setHerramienta("linea");
+        if (e.getSource() == dibujo) {
+            miPanel.setHerramienta("dibujo");
         }
         if (e.getSource() == rectangulo) {
             miPanel.setHerramienta("rectangulo");
@@ -117,11 +148,35 @@ class UMLCreator extends JFrame implements ActionListener {
         if (e.getSource() == elipse) {
             miPanel.setHerramienta("elipse");
         }
+        if (e.getSource() == monigote) {
+            miPanel.setHerramienta("monigote");
+        }
+        if (e.getSource() == estado) {
+            miPanel.setHerramienta("estado");
+        }
+        if (e.getSource() == asociacion) {
+            miPanel.setHerramienta("asociacion");
+        }
+        if (e.getSource() == composicion) {
+            miPanel.setHerramienta("composicion");
+        }
+        if (e.getSource() == generalizacion) {
+            miPanel.setHerramienta("generalizacion");
+        }
+        if (e.getSource() == agregacion) {
+            miPanel.setHerramienta("agregacion");
+        }
+        if (e.getSource() == dependencia) {
+            miPanel.setHerramienta("dependencia");
+        }
         if (e.getSource() == relleno) {
             miPanel.setRelleno(relleno.getState());
         }
         if (e.getSource() == color) {
             miPanel.setColor(colorChooser.showDialog(this, "Seleccione un color", Color.BLACK));
+        }
+        if (e.getSource() == agregarTexto) {
+            miPanel.agregarTexto();
         }
     }
 
@@ -130,8 +185,9 @@ class UMLCreator extends JFrame implements ActionListener {
         Graphics2D g2D;
         int currentX, currentY, oldX, oldY;
         boolean relleno = false;
-        String herramienta = "linea";
+        String herramienta = "dibujo";
         Color color = Color.BLACK;
+        String texto = "";
 
         public MiPanel() {
             this.setBackground(Color.WHITE);
@@ -142,7 +198,7 @@ class UMLCreator extends JFrame implements ActionListener {
                 }
 
                 public void mouseReleased(MouseEvent e) {
-                    if (herramienta.equals("linea")) {
+                    if (herramienta.equals("dibujo")) {
                         g2D.draw(new Line2D.Float(oldX, oldY, e.getX(), e.getY()));
                     } else if (herramienta.equals("rectangulo")) {
                         int width = Math.abs(e.getX() - oldX);
@@ -164,6 +220,120 @@ class UMLCreator extends JFrame implements ActionListener {
                         } else {
                             g2D.draw(new Ellipse2D.Float(x, y, width, height));
                         }
+                    } else if (herramienta.equals("monigote")) {
+                        int width = 25;
+                        int height = 50;
+                        int x = e.getX() - width / 2;
+                        int y = e.getY() - height / 2;
+                        g2D.drawOval(x, y, width, height / 2);  // Cabeza
+                        g2D.drawLine(x + width / 2, y + height / 2, x + width / 2, y + height);  // Cuerpo
+                        g2D.drawLine(x, y + height / 2, x + width, y + height / 2);  // Brazos
+                        g2D.drawLine(x + width / 2, y + height, x, y + height + 25);  // Pierna izquierda
+                        g2D.drawLine(x + width / 2, y + height, x + width, y + height + 25);  // Pierna derecha
+                    } else if (herramienta.equals("estado")) {
+                        int width = Math.abs(e.getX() - oldX);
+                        int height = Math.abs(e.getY() - oldY);
+                        int x = Math.min(oldX, e.getX());
+                        int y = Math.min(oldY, e.getY());
+                        int arcWidth = Math.min(width, height);
+                        int arcHeight = Math.min(width, height);
+                        if (relleno) {
+                            g2D.fill(new RoundRectangle2D.Float(x, y, width, height, arcWidth, arcHeight));
+                        } else {
+                            g2D.draw(new RoundRectangle2D.Float(x, y, width, height, arcWidth, arcHeight));
+                        }
+                    } else if (herramienta.equals("generalizacion")) {
+                        Arrow arrow = new Arrow();
+                        arrow.drawArrow(g2D, oldX, oldY, e.getX(), e.getY());
+                    } else if (herramienta.equals("asociacion" +
+                            "")) {
+                        g2D.drawLine(oldX, oldY, e.getX(), e.getY());
+                    }else if (herramienta.equals("composicion")) {
+                        int width = Math.abs(e.getX() - oldX);
+                        int height = Math.abs(e.getY() - oldY);
+                        int x = Math.min(oldX, e.getX());
+                        int y = Math.min(oldY, e.getY());
+                        int startX = x;
+                        int startY = y + height / 2;
+                        int endX = x + width - height / 2;
+                        int endY = startY;
+                        int diamondX = endX - height / 2;
+                        int diamondY = endY - height / 4;
+                        int diamondWidth = height;
+                        int diamondHeight = height / 2;
+
+                        g2D.draw(new Line2D.Float(startX, startY, endX, endY));
+                        g2D.setColor(Color.BLACK);
+                        g2D.fillPolygon(
+                                new int[]{diamondX, diamondX + diamondWidth / 2, diamondX + diamondWidth, diamondX + diamondWidth / 2},
+                                new int[]{diamondY, diamondY - diamondHeight / 2, diamondY, diamondY + diamondHeight / 2},
+                                4
+                        );
+                        g2D.setColor(color);
+                    }else if (herramienta.equals("agregacion")) {
+                        int width = Math.abs(e.getX() - oldX);
+                        int height = Math.abs(e.getY() - oldY);
+                        int x = Math.min(oldX, e.getX());
+                        int y = Math.min(oldY, e.getY());
+                        int startX = x;
+                        int startY = y + height / 2;
+                        int endX = x + width - height / 2;
+                        int endY = startY;
+                        int diamondX = endX - height / 2;
+                        int diamondY = endY - height / 4;
+                        int diamondWidth = height;
+                        int diamondHeight = height / 2;
+
+                        g2D.draw(new Line2D.Float(startX, startY, endX, endY));
+                        g2D.setColor(Color.BLACK);
+                        g2D.setStroke(new BasicStroke(2));
+                        g2D.drawPolygon(
+                                new int[]{diamondX, diamondX + diamondWidth / 2, diamondX + diamondWidth, diamondX + diamondWidth / 2},
+                                new int[]{diamondY, diamondY - diamondHeight / 2, diamondY, diamondY + diamondHeight / 2},
+                                4
+                        );
+                        g2D.setColor(color);
+                    }else if (herramienta.equals("dependencia")) {
+                        int startX = oldX;
+                        int startY = oldY;
+                        int endX = e.getX();
+                        int endY = e.getY();
+
+                        int arrowLength = 10;  
+                        int arrowSpacing = 5;  
+
+                        double angle = Math.atan2(endY - startY, endX - startX);
+                        double dx = Math.cos(angle) * (arrowLength + arrowSpacing);
+                        double dy = Math.sin(angle) * (arrowLength + arrowSpacing);
+
+                        // Dibuja las secciones de la flecha
+                        boolean isWhiteSection = true;
+                        for (double i = 0; i < Math.sqrt((endX - startX) * (endX - startX) + (endY - startY) * (endY - startY)); i += arrowLength + arrowSpacing) {
+                            int sectionStartX = startX + (int) (i * Math.cos(angle));
+                            int sectionStartY = startY + (int) (i * Math.sin(angle));
+                            int sectionEndX = sectionStartX + (int) (arrowLength * Math.cos(angle));
+                            int sectionEndY = sectionStartY + (int) (arrowLength * Math.sin(angle));
+
+                            if (isWhiteSection) {
+                                g2D.setColor(Color.WHITE);
+                            } else {
+                                g2D.setColor(Color.BLACK);
+                            }
+
+                            g2D.drawLine(sectionStartX, sectionStartY, sectionEndX, sectionEndY);
+                            isWhiteSection = !isWhiteSection;
+                        }
+
+                
+                        int arrowEndX = (int) (endX - arrowLength * Math.cos(angle));
+                        int arrowEndY = (int) (endY - arrowLength * Math.sin(angle));
+                        int[] arrowX = {endX, arrowEndX - 8, arrowEndX - 8, endX};
+                        int[] arrowY = {endY, arrowEndY - 4, arrowEndY + 4, endY};
+                        g2D.setColor(Color.BLACK);
+                        g2D.fillPolygon(arrowX, arrowY, 4);
+                    }
+                    if (!texto.isEmpty()) {
+                        g2D.drawString(texto, oldX, oldY);
                     }
                     repaint();
                 }
@@ -173,7 +343,7 @@ class UMLCreator extends JFrame implements ActionListener {
                 public void mouseDragged(MouseEvent e) {
                     currentX = e.getX();
                     currentY = e.getY();
-                    if (herramienta.equals("linea")) {
+                    if (herramienta.equals("dibujo")) {
                         g2D.drawLine(oldX, oldY, currentX, currentY);
                         repaint();
                         oldX = currentX;
@@ -194,6 +364,10 @@ class UMLCreator extends JFrame implements ActionListener {
         public void setColor(Color color) {
             this.color = color;
             g2D.setColor(color);
+        }
+
+        public void agregarTexto() {
+            texto = JOptionPane.showInputDialog(null, "Ingrese el texto:");
         }
 
         public void abrir() {
@@ -217,7 +391,16 @@ class UMLCreator extends JFrame implements ActionListener {
             if (seleccion == JFileChooser.APPROVE_OPTION) {
                 File archivo = fc.getSelectedFile();
                 try {
-                    ImageIO.write(buffer, "png", archivo);
+                    BufferedImage image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+                    Graphics2D g = image.createGraphics();
+                    g.setColor(Color.WHITE);
+                    g.fillRect(0, 0, getWidth(), getHeight());
+                    paint(g);
+                    String filePath = archivo.getPath();
+                    if (!filePath.toLowerCase().endsWith(".png")) {
+                        filePath += ".png";
+                    }
+                    ImageIO.write(image, "png", new File(filePath));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -233,5 +416,21 @@ class UMLCreator extends JFrame implements ActionListener {
             }
             g.drawImage(buffer, 0, 0, null);
         }
+    }
+}
+class Arrow {
+    private static final int ARROW_SIZE = 10;
+
+    public void drawArrow(Graphics2D g2D, int x1, int y1, int x2, int y2) {
+        g2D.drawLine(x1, y1, x2, y2);
+
+        double angle = Math.atan2(y2 - y1, x2 - x1);
+        int x = (int) (x2 - ARROW_SIZE * Math.cos(angle - Math.PI / 6));
+        int y = (int) (y2 - ARROW_SIZE * Math.sin(angle - Math.PI / 6));
+        g2D.drawLine(x2, y2, x, y);
+
+        x = (int) (x2 - ARROW_SIZE * Math.cos(angle + Math.PI / 6));
+        y = (int) (y2 - ARROW_SIZE * Math.sin(angle + Math.PI / 6));
+        g2D.drawLine(x2, y2, x, y);
     }
 }
